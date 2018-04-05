e90c918d64433dbe9e99f8e003b6f073d97ea1e0
Krittanat <theton0087@hotmail.com>
2018-04-05 14:25:05 +0700

    Convert DB script extenstion to csv and Create README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Get-A.com
+
+
+## Looking a Place for Reading or Work?
+_Coming Soon_
+
+
+## Find a Book for Read
+### Feature
+* Search books by categories or name
+### Work in Process
+* Best books of the week/month/year
+* User can add and delete a book
+* User can vote and cancel vote a book
--- data/book.py
+++ data/book.py
@@ -1,4 +1,6 @@
+import csv
 from book.models import Book, Categories
+
 
 class NameDuplicate(Exception):
     def __init__(self, name):
@@ -6,31 +8,29 @@ class NameDuplicate(Exception):
 
 
 def create_books():
-    f = open('data/all_book.txt', 'r')
+    with open('data/all_book.csv', 'r') as f:
+        reader = csv.reader(f)
+        for book_detail in reader:
+            book_name = book_detail[0]
+            book_score = float(book_detail[1])
+            book_cat = book_detail[-1].split(',')                                # Split category
 
-    for line in f:
-        all_book = [word for word in line.split('\t') if word not in '']         # Split name, score and categories
-        all_book[1] = float(all_book[1])
-        all_book[-1] = all_book[-1][:-1].split(',')                              # Delete /n and split category
+            try:
+                if Book.objects.filter(name__exact=book_name):                   # Check for an existing book
+                    raise NameDuplicate(book_name)
+                else:
+                    b = Book(name=book_name, score=book_score)
+                    b.save()
 
-        try:
-            if Book.objects.filter(name__exact=all_book[0]):                     # Check for an existing book
-                raise NameDuplicate(all_book[0])
-            else:
-                b = Book(name=all_book[0], score=all_book[1])
-                b.save()
+                    for c in book_cat:
+                        if Categories.objects.filter(name__exact=c):             # Check for an existing category
+                            b.categories.add(Categories.objects.get(name=c))
+                        else:
+                            Categories(name=c).save()
+                            b.categories.add(Categories.objects.get(name=c))
 
-                for c in all_book[-1]:
-                    if Categories.objects.filter(name__exact=c):                 # Check for an existing category
-                        b.categories.add(Categories.objects.get(name=c))
-                    else:
-                        Categories(name=c).save()
-                        b.categories.add(Categories.objects.get(name=c))
-
-        except NameDuplicate as n:
-            print('NameDuplicate: There is an existing ' + n.name + ' book')
-
-    f.close()
+            except NameDuplicate as n:
+                print('NameDuplicate: There is an existing ' + n.name + ' book')
 
 
 def delete_all():
